3917ea9ad26b59c2eac8705c64582a2d26963e26
Sammy <segzyfaj1@gmail.com>
2022-08-13 10:10:38 +0100

    A new Update

---
+++ components/data.tsx
@@ -0,0 +1,107 @@
+export const data = [
+    {
+        name: "SamFajobi",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOyCtPSy5vZ6NNKe6nmW5aEAWDfv2R3bV72g&usqp=CAU",
+        available: "false"
+    },
+    {
+        name: "SamFajobi",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOyCtPSy5vZ6NNKe6nmW5aEAWDfv2R3bV72g&usqp=CAU",
+        available: true
+    },
+    {
+        name: "SamFajobi",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOyCtPSy5vZ6NNKe6nmW5aEAWDfv2R3bV72g&usqp=CAU",
+        available: true
+    },
+    {
+        name: "SamFajobi",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOyCtPSy5vZ6NNKe6nmW5aEAWDfv2R3bV72g&usqp=CAU",
+        available: false
+    },
+    {
+        name: "SamFajobi",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-Q8ECTccsWd1sWeH6R8qtw8Q3gms083433w&usqp=CAU",
+        available: true
+    },
+    {
+        name: "SamFajobi",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-Q8ECTccsWd1sWeH6R8qtw8Q3gms083433w&usqp=CAU",
+        available: true
+    },
+    {
+        name: "SamFajobi",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-Q8ECTccsWd1sWeH6R8qtw8Q3gms083433w&usqp=CAU",
+        available: true
+    },
+    {
+        name: "SamFajobi",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-Q8ECTccsWd1sWeH6R8qtw8Q3gms083433w&usqp=CAU",
+        available: false
+    },
+    {
+        name: "SamFajobi",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXRo2_B-7ZcIS_4bsHTZAbMdzhDrab3WzM5w&usqp=CAU",
+        available: true
+    },
+    {
+        name: "SamFajobi",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXRo2_B-7ZcIS_4bsHTZAbMdzhDrab3WzM5w&usqp=CAU",
+        available: false
+    },
+    {
+        name: "SamFajobi",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXRo2_B-7ZcIS_4bsHTZAbMdzhDrab3WzM5w&usqp=CAU",
+        available: true
+    },
+    {
+        name: "SamFajobi",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXRo2_B-7ZcIS_4bsHTZAbMdzhDrab3WzM5w&usqp=CAU",
+        available: false
+    },
+    {
+        name: "SamFajobi",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXRo2_B-7ZcIS_4bsHTZAbMdzhDrab3WzM5w&usqp=CAU",
+        available: false
+    },
+    {
+        name: "SamFajobi",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXRo2_B-7ZcIS_4bsHTZAbMdzhDrab3WzM5w&usqp=CAU",
+        available: false
+    },
+    {
+        name: "SamFajobi",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXRo2_B-7ZcIS_4bsHTZAbMdzhDrab3WzM5w&usqp=CAU",
+        available: false
+    },
+    {
+        name: "SamFajobi",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXRo2_B-7ZcIS_4bsHTZAbMdzhDrab3WzM5w&usqp=CAU",
+        available: false
+    },
+    {
+        name: "SamFajobi",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXRo2_B-7ZcIS_4bsHTZAbMdzhDrab3WzM5w&usqp=CAU",
+        available: false
+    },
+    {
+        name: "SamFajobi",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXRo2_B-7ZcIS_4bsHTZAbMdzhDrab3WzM5w&usqp=CAU",
+        available: false
+    },
+    {
+        name: "SamFajobi",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXRo2_B-7ZcIS_4bsHTZAbMdzhDrab3WzM5w&usqp=CAU",
+        available: false
+    },
+    {
+        name: "SamFajobi",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXRo2_B-7ZcIS_4bsHTZAbMdzhDrab3WzM5w&usqp=CAU",
+        available: false
+    },
+    {
+        name: "SamFajobi",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXRo2_B-7ZcIS_4bsHTZAbMdzhDrab3WzM5w&usqp=CAU",
+        available: false
+    },
+]
--- pages/asas.tsx
+++ pages/asas.tsx
@@ -1,22 +1,42 @@
-export const Asas = () => {
-    return (
-        <div className="pageContainer">
-            <div className="headers">
-                <div className="logo">
-                    <img src="Vector 2.png" alt="" />
-                    <img src="SAlytics.png" alt="" />
-                </div>
-                <div className="button">
-                    <button>ANALYSE ASAs</button>
-                </div>
-                <h1>Hello World</h1>
-            </div>
-            <div className="title">
-                
-            </div>
-        </div>
-    )
+import styles from "../styles/Asas.module.css"
+import { data } from '../components/data'
 
+const Asas = () => {
+    return (
+       
+        <div className={styles.pageContainer}>
+            <div>
+                <div className={styles.headers}>
+                    <div className={styles.logo}>
+                        <img src="Vector 2.png" alt="" />
+                        <img src="SAlytics.png" alt="" />
+                    </div>
+                    <div className={styles.buttonDiv}>
+                        <button className={styles.button}>ANALYSE ASAs</button>
+                    </div>
+                </div>
+                <div className={styles.title}>List of Algorand Standard Assets <br/> on ASAlytics</div>
+                <div className={styles.Asas}>
+                    {data.map((value) => (
+                    <div className={styles.AsaDatas}>
+                        <img className={styles.Logo}src={value.logo}/>
+                        <div className={styles.Name}>{value.name}</div>
+                        <div className={styles.Availability}>{value.available}</div>
+                    </div>
+                    ))}
+                </div>
+            </div>    
+        </div>
+   
+  )
+
+}
+
+const getStaticProps = async () => {
+    const response =  await fetch(" https://analytics-api.herokuapp.com/analytics");
+    const result = await response.json()
+
+    console.log(result)
 }
 
 export default Asas;
