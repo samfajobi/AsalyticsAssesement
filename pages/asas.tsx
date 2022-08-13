fb51a728ce6fbe7cb1b53205f61d5357a5c1fd1b
Sammy <segzyfaj1@gmail.com>
2022-08-13 03:56:47 +0100

    Anew changes

---
+++ pages/asas.tsx
@@ -0,0 +1,22 @@
+export const Asas = () => {
+    return (
+        <div className="pageContainer">
+            <div className="headers">
+                <div className="logo">
+                    <img src="Vector 2.png" alt="" />
+                    <img src="SAlytics.png" alt="" />
+                </div>
+                <div className="button">
+                    <button>ANALYSE ASAs</button>
+                </div>
+                <h1>Hello World</h1>
+            </div>
+            <div className="title">
+                
+            </div>
+        </div>
+    )
+
+}
+
+export default Asas;
